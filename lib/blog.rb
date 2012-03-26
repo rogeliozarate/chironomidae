a3af2f97c9d27879d074d82ca79ddfabcbfd966a
Rogelio Zarate <i.am@rogeliozarate.com>
2012-03-26 16:21:48 -0600

    - article parsing - index generation - views in erb, layout and css

--- lib/blog.rb
+++ lib/blog.rb
@@ -5,12 +5,42 @@
 require 'sinatra/base' 
 require 'ostruct' 
 require 'time'
+require 'yaml'
 
 class Blog < Sinatra::Base 
-# File.expand_path generates an absolute path. 
-# It also takes a path as second argument. 
-#The generated path is treated as being relative to that path. 
+  set :root, File.expand_path('../../', __FILE__)     # File.expand_path generates an absolute path. 
+                                                      # It also takes a path as second argument. 
+                                                      # The generated path is treated as being relative to that path.
+                            
+  set :articles, []
+                                                    
+  Dir.glob "#{root}/articles/*.md" do |file|          # loop through all the article files 
+    
+    meta, content	= File.read(file).split("\n\n", 2)  # parse meta data and content from file 
+   
+    article	= OpenStruct.new YAML.load(meta)          # generate a metadata object 
+    
+    article.date	=Time.parse article.date.to_s       # convert the date to a time object
 
-set :root, File.expand_path('../../', __FILE__)
+    article.content = content                         # add the content 
+
+    article.slug	= File.basename(file, '.md')        # generate a slug for the url 
+    
+   
+    get "/#{article.slug}" do                         # set up the route 
+      erb :post, :locals => { :article => article } 
+    end
+    
+    
+    articles << article                               # Add article to list of articles
+    
+    
+    articles.sort_by! { |article| article.date }      # Sort articles by date, display new articles first 
+    articles.reverse!
+    
+    get '/' do 
+      erb :index
+    end 
+    
+  end
 end
-
